163e83939c19b6441160775e3471164c26fe2cfe
Balusu-Revanth <balusurevanth5@gmail.com>
2024-01-15 19:21:50 +0530

    updated the createNote and updateNote pages to integrate with the backend

--- pages/createNotes/index.js
+++ pages/createNotes/index.js
@@ -1,5 +1,34 @@
-const createNoteInput = document.querySelector(".create-note-input");
+const createNoteButton = document.querySelector(".create-note-button");
 
-createNoteInput.addEventListener("change", (e) => {
-    console.log(e.target.value);
+const apiUrl = "http://localhost:8000";
+
+const token = localStorage.getItem("jwt");
+
+createNoteButton.addEventListener("click", () => {
+    const heading = document.querySelector(".create-note-heading").value;
+    const content = document.querySelector(".create-note-input").value;
+    if (token) {
+        fetch(`${apiUrl}/note/add`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                authorization: token,
+            },
+            body: JSON.stringify({
+                heading,
+                content,
+            }),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                if (data.message) {
+                    location.href = "/pages/dashboard/dashboard.html";
+                }
+            })
+            .catch((err) => {
+                alert("Error Creating Note");
+                console.log(err);
+            });
+    }
 });
--- pages/updateNotes/index.js
+++ pages/updateNotes/index.js
@@ -1,8 +1,57 @@
-const createNoteInput = document.querySelector(".create-note-input");
-
-createNoteInput.addEventListener("change", (e) => {
-    console.log(e.target.value);
-});
-
 const urlParams = new URLSearchParams(window.location.search);
 const noteId = urlParams.get("noteId");
+
+const updateNoteButton = document.querySelector(".update-note-button");
+
+const apiUrl = "http://localhost:8000";
+
+const token = localStorage.getItem("jwt");
+
+fetch(`${apiUrl}/note/getnotebyid/${noteId}`, {
+    method: "GET",
+    headers: {
+        authorization: token,
+    },
+})
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data);
+        if (data.message) {
+            const { heading, content } = data.data[0];
+            document.querySelector(".update-note-heading").value = heading;
+            document.querySelector(".update-note-input").value = content;
+        }
+    })
+    .catch((err) => {
+        alert("Error Fetching Note");
+        console.log(err);
+    });
+
+updateNoteButton.addEventListener("click", () => {
+    const heading = document.querySelector(".update-note-heading").value;
+    const content = document.querySelector(".update-note-input").value;
+    if (token) {
+        fetch(`${apiUrl}/note/update/${noteId}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+                authorization: token,
+            },
+            body: JSON.stringify({
+                heading,
+                content,
+            }),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                if (data.message) {
+                    location.href = "/pages/dashboard/dashboard.html";
+                }
+            })
+            .catch((err) => {
+                alert("Error Updating Note");
+                console.log(err);
+            });
+    }
+});
